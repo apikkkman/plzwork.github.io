38c71424213821aae462eedebfec75dd57ec91c9
apikkkman <arturavetisan8@gmail.com>
2025-04-17 10:16:18 +0300

    Add files via upload

---
+++ success.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Успех</title>
+</head>
+<body>
+    <h1>Форма успешно отправлена!</h1>
+</body>
+</html>
