2971f0eae5cd1c56a080ae7c686519eb25ae9bdb
apikkkman <arturavetisan8@gmail.com>
2025-04-30 17:37:00 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,14 +1,12 @@
 <?php
-// Подключение к базе данных
 $host = 'localhost';
-$db = 'u68669';
-$user = 'u68669'; // замените на ваше имя пользователя
-$pass = '5943600'; // замените на ваш пароль
+$db = 'u68672';
+$user = 'u68672';
+$pass = '5722731';
 
 $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Обработка данных формы
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $phone = $_POST['phone'];
@@ -18,18 +16,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $biography = $_POST['biography'];
     $programming_languages = implode(',', $_POST['programming_languages']);
     $consent = isset($_POST['consent']) ? 1 : 0;
-
-    // Генерация логина и пароля
     $username = strtolower(substr($name, 0, 3) . rand(100, 999));
-    $password = bin2hex(random_bytes(4)); // Генерация случайного пароля
+    $password = bin2hex(random_bytes(4));
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
     $stmt = $pdo->prepare("INSERT INTO users (name, phone, email, gender, birthdate, biography, programming_languages, password_hash, username) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
     
     try {
-        // Выполнение запроса
         $stmt->execute([$name, $phone, $email, $gender, $birthdate, $biography, $programming_languages, $password_hash, $username]);
-
-        // Успешная регистрация
         echo "Регистрация прошла успешно!<br>";
         echo "Ваш логин: $username<br>";
         echo "Ваш пароль: $password<br>";
