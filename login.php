645b3effa12dd8829fd02b91f5baa8656fa4b70d
apikkkman <arturavetisan8@gmail.com>
2025-05-15 05:21:28 +0300

    Update login.php

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($user && password_verify($password, $user['password_hash'])) {
         // Генерация JWT
-        $secret = 'your_secret_key'; // Замените на ваш секретный ключ
+        $secret = 'your_secret_key'; 
         $payload = [
             'id' => $user['id'],
             'username' => $user['username'],
